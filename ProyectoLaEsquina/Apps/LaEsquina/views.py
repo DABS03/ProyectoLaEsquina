4790ee219be3e30b2d967bdb47dfa21069671584
DABS03 <103340654+DABS03@users.noreply.github.com>
2024-09-02 23:06:09 -0600

    Vista vlogin

--- ProyectoLaEsquina/Apps/LaEsquina/views.py
+++ ProyectoLaEsquina/Apps/LaEsquina/views.py
@@ -1,12 +1,4 @@
-'''from django.shortcuts import render
-from django.http import HttpResponse
-
-def hola_mundo(request):
-    return HttpResponse("Hola mundo")'''
-
 from django.shortcuts import render
-from .models import EstadoPedido
 
-def hola_mundo(request):
-    estados = EstadoPedido.objects.all()  # Obtiene todos los estados de pedido
-    return render(request, 'lista_estados.html', {'estados': estados})
+def login_view(request):
+    return render(request, 'vlogin.html')
